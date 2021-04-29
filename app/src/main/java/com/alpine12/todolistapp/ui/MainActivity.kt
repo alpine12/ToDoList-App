1887055dd64a51b52178fe2f97d93f09c23af1c7
alpine12 <iqballecip@gmail.com>
2021-04-29 20:44:31 +0700

    setup navigation component

--- app/src/main/java/com/alpine12/todolistapp/ui/MainActivity.kt
+++ app/src/main/java/com/alpine12/todolistapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.alpine12.todolistapp
+package com.alpine12.todolistapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alpine12.todolistapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/alpine12/todolistapp/ui/task/TaskFragment.kt
@@ -0,0 +1,14 @@
+package com.alpine12.todolistapp.ui.task
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.alpine12.todolistapp.R
+
+class TaskFragment : Fragment(R.layout.fragment_task) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
